fe8c4659454a02382ac1e612a469a6522c8c3f85
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 04:17:55 +0000

    automatically backed up by learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
 
 -- Now alter the table to include hp , default hp to 60
-ALTER TABLE pokemon ADD COLUMN hp TEXT default 60;
+-- ALTER TABLE pokemon ADD COLUMN hp TEXT default 60;
