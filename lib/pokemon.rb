05f7396831783d713b9e4ff3c9b49346570acfce
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 03:38:52 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Pokemon
   attr_accessor :name, :type, :db, :id
 
@@ -26,7 +24,7 @@ class Pokemon
 
     new_pokemon = Pokemon.new()
     pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = '#{id}'; ")
-    Binding.pry
+
     new_pokemon.id = pokemon_array[0]
     new_pokemon.name = pokemon_array[1]
     new_pokemon.type = pokemon_array[2]
