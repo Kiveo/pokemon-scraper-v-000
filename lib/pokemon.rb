6800831087d8eadaca1fcad4cf83bd43004415d0
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 02:54:02 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,9 @@ class Pokemon
   end
 
   def self.find(id, db)
-    db.execute("SELECT * FROM pokemon WHERE id = '#{id}'; ")
+    db.execute("SELECT * FROM pokemon WHERE id = '#{id}'; ").each {
+      |pokemon| pokemon
+    }
     # new_pokemon
   end
 
