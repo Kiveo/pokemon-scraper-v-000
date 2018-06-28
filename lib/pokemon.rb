5e645f650063561e8ccbc844ba758b59c2ba74a9
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 01:41:51 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,16 +8,3 @@ class Pokemon
   end
 
 end
-
-
-#
-# Confused about
-# 1. What is a keyword
-# 2. How does one interact with an initialize method
-
-
-# OK. I didnt have an ID...so I got myself worked up. Lemme try rq please. SO SORRY about the audio
-#
-# LOL forgot id again   ...only diff here was missing id, so lets hope that was it.
-#
-# primary key, got it
