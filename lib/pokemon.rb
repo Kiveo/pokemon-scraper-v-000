e9f62254eed767d43db6ba81c6496fc7f98c9e76
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 04:30:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
 
   @@all = []
 
-  def initialize(name:, type:, db:, id:, hp: nil)
+  def initialize(name:, type:, db:, id:, hp: null)
     @name = name
     @type = type
     @db = db
