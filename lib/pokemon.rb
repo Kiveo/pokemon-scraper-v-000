986d7dbb7f5c266e37d917a94115375521ef6ed0
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 04:09:43 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,7 +24,7 @@ class Pokemon
 
     pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = '#{id}'; ")
     new_pokemon = Pokemon.new(id: pokemon_array[0,0], name: pokemon_array[0,1], type: pokemon_array[0,2], db: db)  #an array? Why?
-    new_pokemon
+    # new_pokemon
   end
 
 end
