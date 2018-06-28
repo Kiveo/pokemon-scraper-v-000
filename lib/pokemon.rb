091b5d10d06385a36ff14e04b4eea5ef75eb0cae
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 01:21:18 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :name, :type
 
-  def initialize(name: "Pocket monster", type: "Animal", db: "Some DB")
+  def initialize(id:, name: "Pocket monster", type: "Animal", db: "Some DB")
     # @name = name
     # @type = type
     # @db = db
