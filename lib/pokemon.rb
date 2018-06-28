61852785cdf061f205c94d9d07d516cb6873f3d8
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 01:38:12 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,17 +1,21 @@
 class Pokemon
-  attr_accessor :name, :type, :db
-
-  def initialize(name:, type:, db:)   
-
+  attr_accessor :name, :type, :db, :id
+  def initialize(name:, type:, db:, id:)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
   end
 
 end
 
 
-
-Confused about
-1. What is a keyword
-2. How does one interact with an initialize method
+#
+# Confused about
+# 1. What is a keyword
+# 2. How does one interact with an initialize method
 
 
 # OK. I didnt have an ID...so I got myself worked up. Lemme try rq please. SO SORRY about the audio
+#
+# LOL forgot id again   ...only diff here was missing id, so lets hope that was it.
