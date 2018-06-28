849a4f5dfed2dae5a07de0a9756e9c2d4b956259
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 01:36:32 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,17 @@
 class Pokemon
-  # attr_accessor :name, :type
+  attr_accessor :name, :type, :db
 
-  def initialize(name: "Pocket monster", type: "Animal", db: "Some DB")
-    # @name = name
-    # @type = type
-    # @db = db
+  def initialize(name:, type:, db:)   
+
   end
 
 end
+
+
+
+Confused about
+1. What is a keyword
+2. How does one interact with an initialize method
+
+
+# OK. I didnt have an ID...so I got myself worked up. Lemme try rq please. SO SORRY about the audio
